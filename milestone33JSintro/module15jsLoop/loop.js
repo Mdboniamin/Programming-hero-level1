65ec928610109b9f33c8f10a7effcdbb5f33e1bb
Boni amin <aminboni070@gmail.com>
2026-04-25 10:19:38 +0600

    conditional and loop basic

---
+++ milestone33JSintro/module15jsLoop/loop.js
@@ -0,0 +1,16 @@
+//recap
+
+const amount = 200;
+
+if(amount > 100){
+
+}
+else{
+
+}
+const numbers = [12, 32, 54, 65];
+console.log(numbers);
+
+for(const num of numbers){
+    console.log(num);
+}
