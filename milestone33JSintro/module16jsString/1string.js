16b95185169a5871b4c65be50f1dda95826a235a
Boni amin <aminboni070@gmail.com>
2026-04-25 19:26:39 +0600

    string immutable

---
+++ milestone33JSintro/module16jsString/1string.js
@@ -0,0 +1,12 @@
+const country = 'Bangladesh';
+const division = "noakhali";
+const district = `Bramman baria`;
+const thana = new String('Demra'); //creates object of string
+
+console.log(typeof thana); 
+
+const capital = 'dhaka';
+
+console.log(capital.length);//returns the length
+capital[0] = 'F';//will not work
+//because string is immutable
