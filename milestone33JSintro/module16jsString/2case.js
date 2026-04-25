0aeb63276f96c7a3f15b8e2c0bda9c7890a85b0a
Boni amin <aminboni070@gmail.com>
2026-04-25 19:51:55 +0600

    js uppercase lowercase trim empty space

---
+++ milestone33JSintro/module16jsString/2case.js
@@ -0,0 +1,25 @@
+const school = 'raj uk uttara Model School';
+
+const subject = 'Chemistry';
+const book = 'chemistry';
+
+if (subject.toLowerCase() === book.toLowerCase()){
+    console.log('I am reading book aibar proikkhai fatai felbo');
+}
+else{
+    console.log('hudai hudai prista ultai ar vat khawar jonno');
+}
+
+//to lowercase
+//to uppercase
+console.log(school.toLowerCase());
+
+//remove empty white space
+const drink = ' water ';
+const liquid = 'water';
+if(drink.trim() === liquid){
+    console.log('pani er opor nam jibon');
+}
+else{
+    console.log('somudre paniache khaite pari na');
+}
