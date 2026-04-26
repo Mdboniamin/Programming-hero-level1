a28b158b357b9dde8ed67f405cb67c3ea0f08d75
Boni amin <aminboni070@gmail.com>
2026-04-26 10:34:09 +0600

    jsKeys Nested keys

---
+++ milestone33JSintro/module16jsString/9keys.js
@@ -0,0 +1,12 @@
+const computer = {
+    brand: 'lenevo',
+    price: 35000,
+    processor: 'intel',
+    hdd: '512gb'
+}
+//finding all keys in object
+const keys = Object.keys(computer);
+console.log(keys)
+//finding all keys values in object
+const values = Object.values(computer);
+console.log(values);//returns all the values of object properties
