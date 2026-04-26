b5ca0b6d8124a3f68ba9e9cb1fa87f86bcb57e28
Boni amin <aminboni070@gmail.com>
2026-04-26 10:01:03 +0600

    js object non primitive

---
+++ milestone33JSintro/module16jsString/6object.js
@@ -0,0 +1,32 @@
+const age = 21;
+const school = 'rifle squre';
+const isPassed = true;
+let isDeveloper;
+
+console.log(isDeveloper);
+
+const subjects = ['Bangla', 'english',
+    'physics', 'Math'];
+const bottle_wrong = ['white', 45]
+const bottleColor = 'white';
+const bottlePrice = 45;
+const bottleDrinks = 'water';
+
+//object
+//non primitive
+const bottle = {
+    brand: 'apple',
+    price: 45,
+    color: 'white',
+    isClean: false
+}
+const subject = {
+    name: 'biology',
+    teacher: 'rasheda mam',
+    examdate: '30may',
+    chapters: ['first', 'second', 'third'],
+    exams: {
+        name: 'final exam',
+        marks: 100
+    }
+}
