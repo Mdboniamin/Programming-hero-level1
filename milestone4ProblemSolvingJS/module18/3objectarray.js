168cb6dba5ecff919be9c9e58b3f34bc24712ff1
Boni amin <aminboni070@gmail.com>
2026-04-28 10:50:16 +0600

    object array loop and access

--- milestone4ProblemSolvingJS/module18/3objectarray.js
+++ milestone4ProblemSolvingJS/module18/3objectarray.js
@@ -1,2 +1,20 @@
 const numbers = [1, 2, 43, 54, 4];
 const players = [{}, {}, {}];
+const employee = [
+    {name: 'ashik', designation: 'content writer', salary: 25000},
+    {name: 'atik', designation: 'developer', salary: 29000},
+    {name: 'abdul rahim', designation: 'digital markete', salary: 21000}
+]
+employee[0].name = 'Ashraf';
+console.log(employee[1].salary)
+for(const employ of employee){
+    console.log(employ);
+}
+for(const employ of employee){
+    console.log(employ.salary);
+}
+for(const employ of employee){
+    const person = emp;
+    const personInfo = person.name +': ' + person.salary;
+    console.log(personInfo);
+}
