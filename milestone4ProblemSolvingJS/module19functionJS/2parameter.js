22c232fa3bf888eb307951e72d9fea32031c8d8b
Boni amin <aminboni070@gmail.com>
2026-05-14 12:53:36 +0600

    function add squre

--- milestone4ProblemSolvingJS/module19functionJS/2parameter.js
+++ milestone4ProblemSolvingJS/module19functionJS/2parameter.js
@@ -4,3 +4,14 @@ function vaatKhao(){
     console.log('plate a khabar dabar nao');
     console.log('gopat gopat kore khao');
 }
+vaatKhao();
+
+function squre(number){
+    console.log(number*number);
+}
+squre(4);
+
+function add(num1, num2){
+    console.log(num1+num2);
+}
+add(5,10);
