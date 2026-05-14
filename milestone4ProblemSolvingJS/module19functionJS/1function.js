bbbbf7324aa2afc553de529654127c7e76f024da
Boni amin <aminboni070@gmail.com>
2026-05-14 12:47:20 +0600

    function parameter

---
+++ milestone4ProblemSolvingJS/module19functionJS/1function.js
@@ -0,0 +1,12 @@
+const number = 56;
+function fanoffkor() {
+    console.log('bosa theke uthe dara')
+    console.log('walk towards the switch')
+    console.log('click the switch to off the fan');
+}
+fanoffkor();
+function brushTeeth(){
+    console.log('pick up the brush');
+    console.log('add paste');
+    console.log('2 min ghoso and goso and fena felo');
+}
---
+++ milestone4ProblemSolvingJS/module19functionJS/2parameter.js
@@ -0,0 +1,6 @@
+function vaatKhao(){
+    console.log('hand wash kore aso');
+    console.log('boso');
+    console.log('plate a khabar dabar nao');
+    console.log('gopat gopat kore khao');
+}
