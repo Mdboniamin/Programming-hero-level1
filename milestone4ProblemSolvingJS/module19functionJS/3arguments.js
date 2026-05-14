849baa6fcc587306f0844add08a1f786026a8542
Boni amin <aminboni070@gmail.com>
2026-05-14 13:01:18 +0600

    function arguments how works

---
+++ milestone4ProblemSolvingJS/module19functionJS/3arguments.js
@@ -0,0 +1,10 @@
+function doubleIt(number){
+    console.log(number*2);
+}
+doubleIt(20);
+console.log('------------------');
+doubleIt(50);
+console.log('------------------');
+doubleIt(100); 
+console.log('------------------');
+doubleIt(500);
